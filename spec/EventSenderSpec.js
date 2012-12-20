49a5b512a9dd67f97c305e385210521e0e8b0fc3
dougbradbury <doug@8thlight.com>
2012-12-20 10:50:47 -0600

    send registration message

--- spec/EventSenderSpec.js
+++ spec/EventSenderSpec.js
@@ -5,8 +5,11 @@ describe("EventSender", function() {
   beforeEach(function() {
     ws = { send: function(data) { } }
     spyOn(ws, "send")
-    builder = { event: function(name, value) {return "message";} }
+    builder = { event: function(name, value) {return "message";},
+      register: function( plays,  hears, device_id, system_version){return "register"}
+    }
     spyOn(builder, "event").andCallThrough();
+    spyOn(builder, "register").andCallThrough();
 
     sender = new EventSender(ws, builder);
   });
@@ -15,6 +18,12 @@ describe("EventSender", function() {
     sender.sendEvent("speed", "55");
     expect(builder.event).toHaveBeenCalledWith("speed", "55");
     expect(ws.send).toHaveBeenCalledWith("000007message");
+  });
+
+  it("should send a registration message", function() {
+    sender.sendRegister(["e1", "e2"], ["e3", "e4"]);
+    expect(builder.register).toHaveBeenCalledWith(["e1", "e2"], ["e3", "e4"], "web", sender.systemVersion);
+    expect(ws.send).toHaveBeenCalledWith("000008register");
   });
 
 });
--- src/EventSender.js
+++ src/EventSender.js
@@ -1,13 +1,21 @@
-function EventSender(web_socket, builder) {
-  this.web_socket = web_socket
-  this.builder = builder
+function EventSender(webSocket, builder) {
+  this.systemVersion = "1.0";
+  this.webSocket = webSocket;
+  this.builder = builder;
 };
 
 EventSender.prototype.sendEvent = function(name, value){
-  var message = this.builder.event(name, value);
-  this.web_socket.send(this.zeroPad(message.length, 6) + message);
+  this.sendMessage(this.builder.event(name, value));
+};
+
+EventSender.prototype.sendRegister = function(plays, hears) {
+  this.sendMessage(this.builder.register(plays, hears, "web", this.systemVersion));
+};
+
+EventSender.prototype.sendMessage = function(message) {
+  this.webSocket.send(this.zeroPad(message.length, 6) + message);
 };
 
 EventSender.prototype.zeroPad = function(number, width) {
   return (new Array(width + 1 - number.toString().length)).join('0') + number;
-}
+};
