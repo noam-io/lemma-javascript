e03fc5bde7d9344d3bd8dfddfd56e12e5678f8ce
Colin Jones <trptcolin@gmail.com>
2014-02-20 16:57:50 -0600

    Bind the disconnect callback so `this` is ok

--- src/ServerLocator.js
+++ src/ServerLocator.js
@@ -7,9 +7,35 @@ if(typeof module !== 'undefined' && module.exports){
   MessageParser = require('./MessageParser');
 }
 
+
+// from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+if(!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+    fToBind = this,
+    fNOP = function () {},
+    fBound = function () {
+      return fToBind.apply(this instanceof fNOP && oThis
+        ? this
+        : oThis,
+        aArgs.concat(Array.prototype.slice.call(arguments)));
+    };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
 function ServerLocator(lemma) {
   this.lemma = lemma;
-  this.lemma.onDisconnectCallback = this.beginLocating;
+  this.lemma.onDisconnectCallback = this.beginLocating.bind(this);
   this.broadcastHost = "255.255.255.255";
   this.broadcastPort = 1030;
   this.dialect = "node.js";
